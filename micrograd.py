960e840ff4d86c03f77552e495258a511fbad6b7
Nithin Vikas <nithinvikas8389@gmail.com>
2025-05-06 19:57:49 +0530

    Added a sample code for understanding

--- micrograd.py
+++ micrograd.py
@@ -2,5 +2,26 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-xs = np.arange(-5, 5, 0.25)
-print(xs)
+class Value:
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Valuedata(data={self.data}, children={self._prev}, op={self._op}, label={self.label})"
+    
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self.label, other.label), '+')
+        return out
+    
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
+
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = a+b; c.label='c'
+print(c)

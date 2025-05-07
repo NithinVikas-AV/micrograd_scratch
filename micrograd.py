74efb4d614710ad3f161adf12e3ff08946e208a6
Nithin Vikas <nithinvikas8389@gmail.com>
2025-05-07 12:36:13 +0530

    Starting the code for micrograd from scratch

--- micrograd.py
+++ micrograd.py
@@ -4,25 +4,18 @@ import matplotlib.pyplot as plt
 from graph import *
 
 class Value:
-    def __init__(self, data, _children=(), _op='', label=''):
-        self.data = data
-        self.grad = 0.0
-        self._prev = set(_children)
-        self._op = _op
-        self.label = label
 
-    def __repr__(self):
-        return f"Valuedata(data={self.data}, children={self._prev}, op={self._op}, label={self.label})"
-    
+    def __init__(self, data):
+        self.data = data
+
     def __add__(self, other):
-        out = Value(self.data + other.data, (self.label, other.label), '+')
+        out = Value(self.data + other.data)
         return out
     
     def __mul__(self, other):
-        out = Value(self.data * other.data, (self, other), '*')
-        return out
+        out = Value(self.data * other.data)
 
-a = Value(2.0, label='a')
-b = Value(-3.0, label='b')
-c = a+b; c.label='c'
-print(c._prev)
+a = Value(5.0)
+b = Value(4.0)
+c = a + b
+print(c)

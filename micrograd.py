b096974f9e78ab19a0f5487e6bf9607bee000080
Nithin Vikas <nithinvikas8389@gmail.com>
2025-05-07 12:37:36 +0530

    Starting the code for micrograd from scratch

--- micrograd.py
+++ micrograd.py
@@ -8,6 +8,9 @@ class Value:
     def __init__(self, data):
         self.data = data
 
+    def __repr__(self):
+        return f"Value( data = {self.data})"
+
     def __add__(self, other):
         out = Value(self.data + other.data)
         return out

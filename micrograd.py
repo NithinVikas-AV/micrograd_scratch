e0ca4d2e7ecb2e43d7a20de3a8c827e4759c2621
Nithin Vikas <nithinvikas8389@gmail.com>
2025-05-07 23:08:04 +0530

    feat: Added sub, neg, rmul, exp, truediv, pow functions

--- micrograd.py
+++ micrograd.py
@@ -17,6 +17,7 @@ class Value:
         return f"Value( data = {self.data}, children = {self._prev}, op = {self._op}, label = {self.label} )"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), _op = '+')
         
         def _backward():
@@ -26,7 +27,14 @@ class Value:
 
         return out
     
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), _op = '*')
 
         def _backward():
@@ -36,6 +44,22 @@ class Value:
 
         return out
     
+    def __rmul__(self, other):
+        return self * other
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __truediv__(self, other):
+        return self * other**-1
+
     def tanh(self):
         x = self.data
         t = ((math.exp(2*x)-1)/(math.exp(2*x)+1))
@@ -44,6 +68,16 @@ class Value:
         def _backward():
             self.grad += (1 - t**2) * out.grad
         out._backward = _backward
+
+        return out
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.grad * out.data
+        out._backward
 
         return out
 
@@ -81,11 +115,8 @@ x2w2 = x2*w2; x2w2.label='x2w2'
 x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label='x1w1x2w2'
 n = x1w1x2w2 + b; n.label='n'
 o = n.tanh(); o.label='o'
-o.grad = 1.0
 
 # Gradients
-z = x1 + x1; z.label='z'
-z.backward()
-
-dot = draw_dot(z)
+o.backward()
+dot = draw_dot(o)
 dot.render('graph_output', format='png', view=True)
